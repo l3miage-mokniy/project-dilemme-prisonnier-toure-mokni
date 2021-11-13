abd7a576a887826a96dbf7dcaae2cd91ba4c7e6d
l3miage-mokniy <yanis.mokni@gmail.com>
2021-11-13 18:09:12 +0100

    VERSION FINALE

--- src/main/java/com/tools/Tools.java
+++ src/main/java/com/tools/Tools.java
@@ -9,11 +9,11 @@ public class Tools {
 	public static int random0_2() {
 		return (int) (Math.random() * (2 - 0 + 1) + 0);
 	}
-	
+
 	public static int random0_1() {
 		return (int) (Math.random() * (1 - 0 + 1) + 0);
 	}
-	
+
 	public static Coup generateCoup50_50() {
 		switch (Tools.random0_1()) {
 		case 0:
@@ -28,59 +28,67 @@ public class Tools {
 	}
 
 	public static Joueur getJoueur(List<Joueur> players, int id_joueur) {
-		int i=0;
-		while(id_joueur != players.get(i).getId()) {
+		int i = 0;
+		while (i<players.size() && id_joueur != players.get(i).getId()) {
 			i++;
 		}
-		if(i<players.size()) {
+		if (i < players.size()) {
 			return players.get(i);
 		} else {
 			return null;
 		}
-		
+
 	}
 
 	public static Rencontre getPartyOpen(List<Rencontre> rencontresOpen, int id_party) {
-		int i=0;
-		while(id_party != rencontresOpen.get(i).getId()) {
-			i++;
-		}
-		if(i<rencontresOpen.size()) {
-			return rencontresOpen.get(i);
+		if (rencontresOpen.size() > 0) {
+			int i = 0;
+			while ( i < rencontresOpen.size() && id_party != rencontresOpen.get(i).getId()) {
+				i++;
+			}
+			if (i < rencontresOpen.size()) {
+				return rencontresOpen.get(i);
+			} else {
+				return null;
+			}
 		} else {
 			return null;
 		}
 	}
 
 	public static Rencontre getPartyClose(List<Rencontre> rencontresClose, int id_party) {
-		int i=0;
-		while(id_party != rencontresClose.get(i).getId()) {
-			i++;
-		}
-		if(i<rencontresClose.size()) {
-			return rencontresClose.get(i);
+		if (rencontresClose.size() > 0) {
+			int i = 0;
+			while (i < rencontresClose.size() && id_party != rencontresClose.get(i).getId()) {
+				i++;
+			}
+			if (i < rencontresClose.size()) {
+				return rencontresClose.get(i);
+			} else {
+				return null;
+			}
 		} else {
 			return null;
 		}
 	}
 
 	public static void closeAGame(Rencontre r, List<Rencontre> rencontresOpen, List<Rencontre> rencontresClosed) {
-		int i=0;
-		while(r != rencontresOpen.get(i)) {
+		int i = 0;
+		while (r != rencontresOpen.get(i)) {
 			i++;
 		}
-		
+
 		rencontresOpen.remove(i);
 		rencontresClosed.add(r);
-		
+
 	}
 
 	public static Rencontre getAParty(int id_party, List<Rencontre> rencontresOpen, List<Rencontre> rencontresClosed) {
 		Rencontre r = null;
-		if(rencontresClosed.size()>0) {
+		if (rencontresClosed.size() > 0) {
 			r = Tools.getPartyClose(rencontresClosed, id_party);
 		}
-		if(r == null && rencontresOpen.size()>0) {
+		if (r == null && rencontresOpen.size() > 0) {
 			r = Tools.getPartyOpen(rencontresOpen, id_party);
 		}
 		return r;
--- src/main/java/data/object/Rencontre.java
+++ src/main/java/data/object/Rencontre.java
@@ -2,6 +2,8 @@ package data.object;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import com.tools.Coup;
 
 public class Rencontre {
@@ -12,7 +14,7 @@ public class Rencontre {
 
 	private int id;
 	private int nb_tours;
-	private int current_tour;
+	private int current_tour = 1;
 	public List<Coup> coupJ1;
 	public List<Coup> coupJ2;
 	public int score1 = 0;
@@ -32,31 +34,6 @@ public class Rencontre {
 		this.createur = createur;
 	}
 
-	/*
-	 * public void startGame() { for(int i = 0; i<nb_tours; i++) { this.newTurn(); }
-	 * System.out.println("Score joueur1 = "+this.score1+" Score joueur 2 = "+this.
-	 * score2); }
-	 
-
-	private void newTurn() {
-		if (createur.isHaveLeave()) {
-			this.coupJ1.add(createur.getStrategy().play(coupJ1, coupJ2));
-		} else {
-			// @TODO => LAISSER CHOIX POSSIBLE
-			System.out.println("Veuillez saisir votre coup : ");
-		}
-
-		if (joueur2.isHaveLeave()) {
-			this.coupJ2.add(joueur2.getStrategy().play(coupJ2, coupJ1));
-		} else {
-			// @TODO => LAISSER CHOIX POSSIBLES
-			System.out.println("Veuillez saisir votre coup : ");
-		}
-
-		updateScore(this.coupJ1.get(this.coupJ1.size() - 1), this.coupJ2.get(this.coupJ2.size() - 1));
-	}
-*/
-	
 	public boolean joinParty(Joueur joueur2) {
 		if (this.joueur2 == null) {
 			this.joueur2 = joueur2;
@@ -66,60 +43,11 @@ public class Rencontre {
 		}
 	}
 
-	public int canPlay() {
-		if (nb_tours == current_tour) {
-			return -2;
-		} else if (this.joueur2 == null) {
-			return -1;
-		} else if (coupJ1.size() != coupJ2.size()) {
-			return 0;
-		} else {
-			return 1;
-		}
-	}
-
-	private void updateScore(Coup coup1, Coup coup2) {
-		if (coup1 == coup2 && coup1 == Coup.COOPERER) {
-			this.score1 += C;
-			this.score2 += C;
-		} else if (coup1 == coup2 && coup1 == Coup.TRAHIR) {
-			this.score1 += P;
-			this.score2 += P;
-		} else if (coup1 != coup2 && coup1 == Coup.TRAHIR) {
-			this.score1 += T;
-			this.score2 += D;
-		} else {
-			this.score1 += D;
-			this.score2 += T;
-		}
-	}
-
-	public Coup[] getTurn(int turn) {
-		if ((turn <= this.nb_tours && turn > 0) && turn < current_tour) {
-			return new Coup[] { this.coupJ1.get(turn - 1), this.coupJ2.get(turn - 1) };
-		} else {
-			return new Coup[] { null, null };
-		}
-	}
-
-	public String getScore() {
-		return this.createur.getName() + " : " + this.score1 + " point(s) - " + this.joueur2.getName() + " : "
-				+ this.score2 + " point(s)";
-	}
-
 	public void leave(Joueur j) {
 		if (j == this.createur) {
 			this.haveLeaveJ1 = true;
 		} else if (j == this.joueur2) {
 			this.haveLeaveJ2 = true;
-		}
-	}
-	
-	public boolean haveJoin() {
-		if(this.joueur2 == null) {
-			return false;
-		} else {
-			return true;
 		}
 	}
 
@@ -142,4 +70,92 @@ public class Rencontre {
 	public Joueur getJoueur2() {
 		return joueur2;
 	}
+
+	public String play(int id_joueur, Coup c) {
+		boolean haveWait = false;
+
+		if (id_joueur == this.createur.getId()) {
+			this.coupJ1.add(c);
+			if (haveLeaveJ2) {
+				this.coupJ2.add(joueur2.getStrategy().play(coupJ2, coupJ1));
+			}
+		} else {
+			this.coupJ2.add(c);
+			if (haveLeaveJ1) {
+				this.coupJ1.add(createur.getStrategy().play(coupJ1, coupJ2));
+			}
+		}
+
+		while (!this.bothHavePlay()) {
+			if (!haveWait) {
+				haveWait = true;
+			}
+		}
+
+		if (haveWait) {
+			try {
+				TimeUnit.SECONDS.sleep(1);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			updateScore(this.coupJ1.get(this.coupJ1.size() - 1), this.coupJ2.get(this.coupJ2.size() - 1));
+			current_tour++;
+		}
+		System.out.println("PLAY");
+		if(this.gameFinished()) {
+			if (score1==score2) {
+				return "EGALITE : "+this.createur.getName() + " a un score de : " + this.score1 + " point(s) - " +
+				this.joueur2.getName() + " a  un score de : " + this.score2 + " point(s)";
+			}else if(this.score1>this.score2) {
+				return "VICTOIRE DE : "+this.createur.getName() + " avec un score de : " + this.score1 + " point(s). " +
+				this.joueur2.getName() + " a  un score de : " + this.score2 + " point(s)";
+			} else {
+				return "VICTOIRE DE : "+this.joueur2.getName() + " avec un score de : " + this.score2 + " point(s). " +
+				this.createur.getName() + " a  un score de : " + this.score1 + " point(s)";
+			}
+		} else {
+			return this.getScore(this.coupJ1.get(this.coupJ1.size() - 1), this.coupJ2.get(this.coupJ2.size() - 1));
+		}
+	}
+
+	private boolean bothHavePlay() {
+		return this.coupJ1.size() == this.coupJ2.size();
+	}
+
+	public String getScore(Coup coupCreateur, Coup coupJoueur2) {
+		return this.createur.getName() + " a joué " + coupCreateur + " et a désormais un score de : " + this.score1
+				+ " point(s) - " + this.joueur2.getName() + " a joué " + coupJoueur2 + " et a désormais un score de : "
+				+ this.score2 + " point(s)";
+	}
+
+	private void updateScore(Coup coup1, Coup coup2) {
+		if (coup1 == coup2 && coup1 == Coup.COOPERER) {
+			this.score1 += C;
+			this.score2 += C;
+		} else if (coup1 == coup2 && coup1 == Coup.TRAHIR) {
+			this.score1 += P;
+			this.score2 += P;
+		} else if (coup1 != coup2 && coup1 == Coup.TRAHIR) {
+			this.score1 += T;
+			this.score2 += D;
+		} else {
+			this.score1 += D;
+			this.score2 += T;
+		}
+	}
+
+	public boolean haveJoin() {
+		if (this.joueur2 == null) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	public boolean gameFinished() {
+		return this.nb_tours+1 == this.current_tour;
+	}
+	
 }
